19e548a082ef01f1f7a5aefe7364a1a1a1db1a17
Mido Reigh <midoreigh@gmail.com>
2017-04-06 00:45:58 +0530

    public

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const app = express();
 app.set('port', PORT);
 app.listen(app.get('port'));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 console.log("I'm wating for you @" + PORT);
 
 // index. Let's say something fun
@@ -108,7 +109,7 @@ app.post('/webhook', (req, res) => {
       // This will run all actions until our bot has nothing left to do
       wit.runActions(
         sessionId, // the user's current session
-        msg, // the user's message 
+        msg, // the user's message
         sessions[sessionId].context, // the user's current session state
         (error, context) => {
           if (error) {
